ec531c3dc5b8d0b5a79659ae63d5932128c7dd6d
Ebenezer Mathebula <mathebu@amazon.com>
2024-05-14 16:54:04 +0200

    Refactor: Changed server from express-graphql -> Apollo server

--- apps/api/lib/server.ts
+++ apps/api/lib/server.ts
@@ -1,10 +1,18 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import {MongoDbClient} from './clients';
-import {graphqlHTTP} from 'express-graphql';
-import {API_PORT, API_DOMAIN, NODE_ENV, STAGES, MONGO_DB_URL} from './constants';
+import { MongoDbClient } from './clients';
+import { API_PORT, API_DOMAIN, NODE_ENV, STAGES, MONGO_DB_URL } from './constants';
 import createSchema from './graphql/schema';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import http from 'http';
+import cors from 'cors';
+
+interface ServerContext {
+    token?: string;
+};
 
 const app: Express = express();
 
@@ -13,26 +21,33 @@ const initializeApp = async () => {
         await MongoDbClient.connectToDatabase(MONGO_DB_URL);
 
         app.use(morgan('dev'));
-        app.use(bodyParser.json({limit: '50mb'}));
-        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+        app.use(bodyParser.json({ limit: '50mb' }));
+        app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-        app.use(
-            '/api/v1/graphql',
-            graphqlHTTP({
-                schema: await createSchema(),
-                graphiql: NODE_ENV === STAGES.DEV,
-            }),
-        );
+        const server = new ApolloServer<ServerContext>({
+            schema: await createSchema(),
+            plugins: [
+                ApolloServerPluginDrainHttpServer({
+                    httpServer:  http.createServer(app)
+                }),
+            ],
+        });
+
+        await server.start();
+
+        app.use('/api/v1/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server, {
+            context: async ({ req }) => ({ token: req.headers.token }),
+        }));
 
         app.listen(API_PORT, () => {
             console.log(`⚡️[server]: Server is running at ${API_DOMAIN}:${API_PORT}`);
             app.emit('appInitialized');
         });
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error('Error while starting the server:', error);
     }
 };
 
 initializeApp();
 
-export {app};
+export { app };
